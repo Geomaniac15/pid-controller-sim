5f39d7de7923f3c663eb11545a46339c1af9da40
George <gveal07@outlook.com>
2026-03-29 18:25:51 +0100

    static plotting

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import matplotlib.pyplot as plt 
+import numpy as np
 
 pos = [0.0, 0.0]
 velocity = [2.0, 2.0]
 acceleration = [0.0, 0.0]
+
+x_pos = []
+y_pos = []
 
 dt = 0.01
 
@@ -13,6 +17,19 @@ for step in range(100):
     pos[0] += velocity[0] * dt
     pos[1] += velocity[1] * dt
 
-    print(f'Position: ({pos[0]},{pos[1]})')
-    print(f'Velocity: ({velocity[0]},{velocity[1]})')
-    print()
+    x_pos.append(pos[0])
+    y_pos.append(pos[1])
+
+    # print(f'Position: ({pos[0]},{pos[1]})')
+    # print(f'Velocity: ({velocity[0]},{velocity[1]})')
+    # print()
+
+xpoints = np.array(x_pos)
+ypoints = np.array(y_pos)
+
+plt.scatter(xpoints, ypoints, s=2)
+
+plt.xlabel('X Velocity')
+plt.ylabel('Y Velocity')
+
+plt.show()
