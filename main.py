17b813c36c36c77b4c5753f4c2c64e59cb14f72c
George <gveal07@outlook.com>
2026-03-30 17:16:39 +0100

    fixed 3d

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from matplotlib.animation import FuncAnimation
 from math import cos, sin, sqrt, inf, pi
 from statistics import mean
 import random
+from mpl_toolkits.mplot3d import Axes3D
 
 pos = [0.0, 0.0, 0.0]
 velocity = [0.0, 0.0, 0.0]
@@ -19,8 +20,11 @@ Ki = 0.06
 
 noisy_x_pos = []
 noisy_y_pos = []
+noisy_z_pos = []
+
 measured_x_pos = []
 measured_y_pos = []
+measured_z_pos = []
 
 alpha = 0.19
 dt = 0.01
@@ -29,6 +33,7 @@ limit = 5
 # initialise filtered state
 filtered_x = 0.0
 filtered_y = 0.0
+filtered_z = 0.0
 
 # error of sums
 error_sum_x = 0.0
@@ -112,6 +117,7 @@ def step_system(pos, velocity, filtered_x, filtered_y, filtered_z, error_sum_x, 
         scale = max_speed / speed
         velocity[0] *= scale
         velocity[1] *= scale
+        velocity[2] *= scale
 
     pos[0] += velocity[0] * dt
     pos[1] += velocity[1] * dt
@@ -128,8 +134,19 @@ def step_system(pos, velocity, filtered_x, filtered_y, filtered_z, error_sum_x, 
 
     ax_total = ax + wind_x + drag_x
     ay_total = ay + wind_y + drag_y
+    az_total = az + wind_z + drag_z
 
-    return pos, velocity, filtered_x, filtered_y, error_sum_x, error_sum_y, x_target, y_target, measured_x, measured_y, ax_total, ay_total, wind_x, wind_y, ax, drag_x, drag_y
+    return (
+        pos, velocity,
+        filtered_x, filtered_y, filtered_z,
+        error_sum_x, error_sum_y, error_sum_z,
+        x_target, y_target, z_target,
+        measured_x, measured_y, measured_z,
+        ax_total, ay_total, az_total,
+        wind_x, wind_y, wind_z,
+        ax, ay, az,
+        drag_x, drag_y, drag_z,
+    )
 
 def simulate(Kp, Kd, Ki=0.0, alpha=0.1):
     # reset state for each simulation
@@ -156,19 +173,38 @@ def simulate(Kp, Kd, Ki=0.0, alpha=0.1):
     for step in range(1000):
         t = step * dt
 
-        pos, velocity, filtered_x, filtered_y, filtered_z, error_sum_x, error_sum_y, error_sum_z, x_target, y_target, z_target, measured_x, measured_y, ax_total, ay_total, wind_x, wind_y, ax, drag_x, drag_y = step_system(
-            pos, velocity, filtered_x, filtered_y, filtered_z, error_sum_x, error_sum_y, error_sum_z
+        (
+            pos, velocity,
+            filtered_x, filtered_y, filtered_z,
+            error_sum_x, error_sum_y, error_sum_z,
+            x_target, y_target, z_target,
+            measured_x, measured_y, measured_z,
+            ax_total, ay_total, az_total,
+            wind_x, wind_y, wind_z,
+            ax, ay, az,
+            drag_x, drag_y, drag_z,
+        ) = step_system(
+            pos, velocity, filtered_x, filtered_y, filtered_z,
+            error_sum_x, error_sum_y, error_sum_z,
             Kp, Kd, Ki, alpha, t, mass, noise_level, wind_scale
         )
 
         # rewards closeness to target, quick reactions, low lag
-        total_cost += (x_target - pos[0])**2 + (y_target - pos[1])**2
-        
+        total_cost += (
+            (x_target - pos[0])**2 +
+            (y_target - pos[1])**2 +
+            0.5 * (z_target - pos[2])**2
+        )
+
         # rewards slower movements, smoothness, low aggressive acceleration
-        total_cost += 0.05 * (velocity[0]**2 + velocity[1]**2)
+        total_cost += 0.05 * (
+            velocity[0]**2 + velocity[1]**2 + velocity[2]**2
+        )
 
         # penalises aggressive control inputs
-        total_cost += 0.01 * (ax_total**2 + ay_total**2)
+        total_cost += 0.01 * (
+            ax_total**2 + ay_total**2 + az_total**2
+        )
 
         # success conditions
         distance = sqrt(
@@ -177,7 +213,7 @@ def simulate(Kp, Kd, Ki=0.0, alpha=0.1):
             (z_target - pos[2])**2
             )
         
-        if distance < 0.1:
+        if distance < 0.2:
             on_target_steps += 1
         else:
             on_target_steps = 0
@@ -195,48 +231,60 @@ def simulate(Kp, Kd, Ki=0.0, alpha=0.1):
 def update(frame):
     t = (frame * dt) % (2 * pi)
 
-    global filtered_x, filtered_y, error_sum_x, error_sum_y
+    global filtered_x, filtered_y, filtered_z, error_sum_x, error_sum_y, error_sum_z
 
     mass = 3.0
     noise_level = 0.2
     wind_scale = 0.4
 
-    pos[:], velocity[:], filtered_x, filtered_y, error_sum_x, error_sum_y, x_target, y_target, measured_x, measured_y, ax_total, ay_total, wind_x, wind_y, ax, drag_x, drag_y = step_system(
-        pos, velocity, filtered_x, filtered_y, error_sum_x, error_sum_y,
+    (
+        pos[:], velocity[:],
+        filtered_x, filtered_y, filtered_z,
+        error_sum_x, error_sum_y, error_sum_z,
+        x_target, y_target, z_target,
+        measured_x, measured_y, measured_z,
+        ax_total, ay_total, az_total,
+        wind_x, wind_y, wind_z,
+        ax, ay, az,
+        drag_x, drag_y, drag_z,
+    ) = step_system(
+        pos, velocity, filtered_x, filtered_y, filtered_z,
+        error_sum_x, error_sum_y, error_sum_z,
         Kp, Kd, Ki, alpha, t, mass, noise_level, wind_scale
     )
 
     noisy_x_pos.append(pos[0])
     noisy_y_pos.append(pos[1])
+    noisy_z_pos.append(pos[2])
+
     measured_x_pos.append(measured_x)
     measured_y_pos.append(measured_y)
+    measured_z_pos.append(measured_z)
 
     line.set_data(noisy_x_pos, noisy_y_pos)
+    line.set_3d_properties(noisy_z_pos)
+
     measured_line.set_data(measured_x_pos, measured_y_pos)
+    measured_line.set_3d_properties(measured_z_pos)
+
     target_point.set_data([x_target], [y_target])
+    target_point.set_3d_properties([z_target])
 
-    wind_vec.set_offsets([pos[0], pos[1]])
-    wind_vec.set_UVC(wind_x, wind_y)
-
-    control_vec.set_offsets([pos[0], pos[1]])
-    control_vec.set_UVC(ax_total, ay_total)
-
-    drag_vec.set_offsets([pos[0], pos[1]])
-    drag_vec.set_UVC(drag_x, drag_y)
-
-    return line, measured_line, target_point, wind_vec, control_vec
+    return line, measured_line, target_point
 
 def animate_controller():
     ax.set_xlim(0, 5)
     ax.set_ylim(0, 5)
+    ax.set_zlim(0, 3)
 
     ax.legend(['Actual Path', 'Measured Path', 'Target'])
 
     ani = FuncAnimation(fig, update, frames=int(2*pi / dt), interval=20, blit=False)
     # ani.save('output.mp4', fps=30)
 
-    plt.xlabel('X Pos')
-    plt.ylabel('Y Pos')
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
 
     plt.show()
 
@@ -280,17 +328,13 @@ def optimise():
 
 def run():
     global fig, ax, line, measured_line, target_point
-    global wind_vec, control_vec, drag_vec
 
-    fig, ax = plt.subplots()
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
 
-    line, = ax.plot([], [])
-    measured_line, = ax.plot([], [], 'g--', alpha=0.6)
-    target_point, = ax.plot([], [], 'ro', markersize=5)
-
-    wind_vec = ax.quiver(0, 0, 0, 0, color='blue', scale=5)
-    control_vec = ax.quiver(0, 0, 0, 0, color='green', scale=5)
-    drag_vec = ax.quiver(0, 0, 0, 0, color='red', scale=5)
+    line, = ax.plot([], [], [], label='Actual Path')
+    measured_line, = ax.plot([], [], [], 'g--', alpha=0.6, label='Measured Path')
+    target_point, = ax.plot([], [], [], 'ro', markersize=5, label='Target')
 
     animate_controller()
 
